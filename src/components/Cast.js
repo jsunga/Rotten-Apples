7d5926c33ed32acd7c3118be8b3bfad14e581277
jsunga <jawyn.sunga@gmail.com>
2019-07-20 16:15:29 -0700

    add cast for movies

---
+++ src/components/Cast.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styling/cast.scss'
+
+const frontPath = 'https://image.tmdb.org/t/p/w185'
+
+const Cast = props => {
+
+    return (
+        <div className='cast-container'>
+            {props.cast.map(item => (
+                <div className='card' key={item.cast_id}>
+                    <div className='image-container'><img src={frontPath + item.profile_path} alt="profile" /></div>
+                    <div className='body'>
+                        <div>{item.name}</div>
+                        <section>{item.character}</section>
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Cast
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Cast from './Cast'
 import axios from 'axios'
 import '../styling/details.scss'
 
@@ -8,6 +9,7 @@ export default class Details extends Component {
 
     state = {
         details: [],
+        cast: [],
         genre1: '',
         genre2: '',
         url: ''
@@ -20,13 +22,21 @@ export default class Details extends Component {
 
     fetchDetails = async handle => {
         let details = await axios.get(`https://api.themoviedb.org/3/movie/${handle}?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US`)
-        this.setState({
-            details: details.data,
-            genre1: details.data.genres[0].name,
-            genre2: details.data.genres[1].name
-        })
+        if (details.data.genres.length === 1) this.setState({details: details.data, genre1: details.data.genres[0].name})
+        else {
+            this.setState({
+                details: details.data,
+                genre1: details.data.genres[0].name,
+                genre2: details.data.genres[1].name
+            })
+        }
+
         let trailers = await axios.get(`https://api.themoviedb.org/3/movie/${handle}/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US`)
         this.setState({url: trailers.data.results[0].key})
+
+        let cast = await axios.get(`https://api.themoviedb.org/3/movie/${handle}/credits?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US`)
+        if (cast.data.cast.length < 12) this.setState({cast: cast.data.cast.slice(0, cast.data.cast.length)})
+        else this.setState({cast: cast.data.cast.slice(0, 12)})
     }
 
     render() {
@@ -42,7 +52,7 @@ export default class Details extends Component {
                         <div className='right-container'>
                             <h1>{details.title}</h1>
                             <section>{details.vote_average} / 10</section>
-                            <div>{genre1} | {genre2}</div>
+                            <div>{genre1} {this.state.genre2 ? '|' : null} {genre2}</div>
                             <main>{`${details.status}`.toUpperCase()} | {`${details.original_language}`.toUpperCase()}</main>
                         </div>
                     </div>
@@ -61,9 +71,7 @@ export default class Details extends Component {
                 </div>
                 <div className='cast'>
                     <h1>CAST</h1>
-                    <main>
-                        hello there
-                    </main>
+                    <Cast cast={this.state.cast} />
                 </div>
             </div>
         )
