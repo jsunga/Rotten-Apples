b3cfb08159d82afabdd777ed30eca2a07e766dbd
jsunga <jawyn.sunga@gmail.com>
2019-09-14 23:49:38 -0700

    Updates

---
+++ precache-manifest.f4257d7cbb6b4a07ea114c7abbb2bed3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aaafced40b325baa73585c7f3476ab1",
+    "url": "/Rotten-Apples/index.html"
+  },
+  {
+    "revision": "0ae2810e4c05b2d9b688",
+    "url": "/Rotten-Apples/static/css/main.e217766f.chunk.css"
+  },
+  {
+    "revision": "96433762e2dfdd56b8fb",
+    "url": "/Rotten-Apples/static/js/2.d8ba002b.chunk.js"
+  },
+  {
+    "revision": "0ae2810e4c05b2d9b688",
+    "url": "/Rotten-Apples/static/js/main.f9e5d8c6.chunk.js"
+  },
+  {
+    "revision": "5d2183521af4d4cd3b1b",
+    "url": "/Rotten-Apples/static/js/runtime~main.ff43c153.js"
+  }
+]);
